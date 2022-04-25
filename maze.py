21d51027243a34a41232863e6672fbab2a0d4364
Lara <lara1907714@miuegypt.edu.eg>
2022-04-25 12:50:58 +0200

    final

--- maze.py
+++ maze.py
@@ -275,7 +275,7 @@ pygame.display.set_caption("Solving a Maze Using AI Algorithms")
 screen = pygame.display.set_mode((750, 450))
 clock = pygame.time.Clock()
 none = True
-drawOnce=False
+drawOnce = False
 walls = []
 path = []
 exp = []
